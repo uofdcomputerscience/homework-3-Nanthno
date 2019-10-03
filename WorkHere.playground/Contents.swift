90289102e61f86b65e8b20f73bb26663f9833e45
Anthony Hanson <ahanson@udallas.edu>
2019-10-03 13:56:20 -0700

    make the project perform per instructions

--- WorkHere.playground/Contents.swift
+++ WorkHere.playground/Contents.swift
@@ -10,3 +10,29 @@ import Foundation
 // decode those 50 words into an array of strings
 // print the array of words, but with each word reversed
 // print the average number of characters per word
+
+struct wordStruct: Codable {
+    let words: [String]
+}
+
+let urlString = "https://api.noopschallenge.com/wordbot?count=50"
+let session = URLSession(configuration: .ephemeral)
+let task = session.dataTask(with: URL(string: urlString)!) {
+    (data, response, error) in
+    if let data = data {
+        let decoder = JSONDecoder()
+        let wordArr = try! decoder.decode(wordStruct.self, from: data)
+        var newWordArr = [String]()
+        var totalLength = 0
+        for word in wordArr.words {
+            newWordArr.append(String(word.reversed()))
+            totalLength += word.count
+        }
+        let averageLength = totalLength/newWordArr.count
+        
+        print(newWordArr)
+        print(averageLength)
+    }
+}
+
+task.resume()
